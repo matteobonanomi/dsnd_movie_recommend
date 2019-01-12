6402078b8a0fd04b237139ab91471c1d25bee887
matteobonanomi <mat.bon92@gmail.com>
2019-01-12 15:32:54 +0100

    [DOC] Add basic README

---
+++ README.m
@@ -0,0 +1,4 @@
+# Build your first Recommendation Engine with Python
+These simple Jupyter Notebook will help you build a recommendation system that suggest a user a list of recommended movies based on other users' visualizations.
+The approach is a collaborative filtering recommendation engine, where each user/movie interacion is compared to all the others user/movie interactions. 
+A certain movie is recommended to a certain user only if it has been high-ranked by users with similar movies preferences.
